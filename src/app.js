f7e6944c601d8b00211a7302759b29fec88c6060
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-25 00:32:06 +0200

    Add default api key for development

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const DarkSkyApi = require('./DarkSkyApi')
+const apikey = require('./apikey')
 
 const app = express()
 
@@ -8,7 +9,7 @@ const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listening on localhost:${port}`))
 
 app.get('/', async (req, res) => {
-	const darkSkyApi = new DarkSkyApi(process.env.API_KEY)
+	const darkSkyApi = new DarkSkyApi(process.env.API_KEY || apikey)
 	const forecast = await darkSkyApi.getForecast('Kyiv')
 	res.send(forecast)
 })
