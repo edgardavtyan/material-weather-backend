02e665077f8de7e0e07f7650299d0c497d67ea43
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-25 00:25:35 +0200

    Get api key from environment var

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listening on localhost:${port}`))
 
 app.get('/', async (req, res) => {
-	const darkSkyApi = new DarkSkyApi('de12b3603c4b1ddc642f1c5a17643dc3')
+	const darkSkyApi = new DarkSkyApi(process.env.API_KEY)
 	const forecast = await darkSkyApi.getForecast('Kyiv')
 	res.send(forecast)
 })
